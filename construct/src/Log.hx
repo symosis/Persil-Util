8e0103af25190a9805ddcfbd1878022adc0e02b7
Patrick Kulling <patrick@patricks-macbook-pro.local>
2012-05-02 13:34:14 +0200

    make sure the logger is initialized

--- construct/src/Log.hx
+++ construct/src/Log.hx
@@ -6,16 +6,25 @@ class Log
 
 	public static inline function debugger()
 	{
+		if(logger == null)
+			init();
+
 		logger.debugger();
 	}
 		
 	public static inline function profile(title : String)
 	{
+		if(logger == null)
+			init();
+			
 		logger.profile(title);
 	}
 		
 	public static inline function profileEnd()
 	{
+		if(logger == null)
+			init();
+			
 		logger.profileEnd();
 	}
 		
@@ -30,46 +39,73 @@ class Log
 
 	public static function addFilter(filter : LogFilter)
 	{
+		if(logger == null)
+			init();
+			
 		logger.addFilter(filter);
 	}
 	
 	public static inline function info(?m0 : Dynamic, ?m1 : Dynamic, ?m2 : Dynamic, ?m3 : Dynamic, ?m4 : Dynamic, ?m5 : Dynamic, ?m6 : Dynamic, ?i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		logger.info(m0, m1, m2, m3, m4, m5, m6, i);
 	}
 	
 	public static inline function warn(?m0 : Dynamic, ?m1 : Dynamic, ?m2 : Dynamic, ?m3 : Dynamic, ?m4 : Dynamic, ?m5 : Dynamic, ?m6 : Dynamic, ?i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		logger.warn(m0, m1, m2, m3, m4, m5, m6, i);
 	}
 	
 	public static inline function error(?m0 : Dynamic, ?m1 : Dynamic, ?m2 : Dynamic, ?m3 : Dynamic, ?m4 : Dynamic, ?m5 : Dynamic, ?m6 : Dynamic, ?i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		logger.error(m0, m1, m2, m3, m4, m5, m6, i);
 	}
 	
 	public static function infoEnabled(i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		return logger.infoEnabled(i);
 	}
 	
 	public static function warnEnabled(i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		return logger.warnEnabled(i);
 	}
 	
 	public static function errorEnabled(i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		return logger.errorEnabled(i);
 	}
 	
 	public static inline function groupCollapsed(?m0 : Dynamic, ?m1 : Dynamic, ?m2 : Dynamic, ?m3 : Dynamic, ?m4 : Dynamic, ?m5 : Dynamic, ?m6 : Dynamic, ?i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		logger.groupCollapsed(m0, m1, m2, m3, m4, m5, m6, i);
 	}
 	
 	public static function groupEnd(?i : haxe.PosInfos)
 	{
+		if(logger == null)
+			init();
+			
 		logger.groupEnd(i);
 	}	
 }
